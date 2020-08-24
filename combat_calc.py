e3eff3d4c96b107d112d77e85ca7b5e243ad99f2
Farhan Ahmad <fahmad010200@gmail.com>
2020-08-24 20:59:18 +0100

    Nvm lmao we're doing it this way anyway DPS Calc is wrong

--- combat_calc.py
+++ combat_calc.py
@@ -78,15 +78,17 @@ def calcAttackRoll(effective_level, equipment_bonus, special_attack = "", mage_l
 		max_roll = multiplyDown(max_roll, 2.00)
 	elif special_attack == "Twisted bow":
 		max_roll = multiplyDown(max_roll, min((0.399 + 0.0063 * min((350, mage_level)) - 0.000009 * min((350, mage_level))**2), 1.4)) #Bow accuracy capped at 1.4	
-	print("Max Attack roll:", max_roll)
 	return max_roll
 
 def calcDefenceRoll(effective_level, equipment_bonus):
 	max_roll = effective_level * (equipment_bonus + 64)
-	print("Max Defence roll:", max_roll)
 	return max_roll
 
-def calcHitChance(max_attack_roll, max_defence_roll):
+def calcHitChance(max_attack_roll, max_defence_roll, brimstone = False):
+	if brimstone:
+		brim_proc = random.randint(1,4)
+		if brim_proc == 1:
+			max_defence_roll = multiplyDown(max_defence_roll, 0.9)
 	if max_attack_roll > max_defence_roll:
 		hit_chance = 1 - (max_defence_roll + 2) / (2 * (max_attack_roll + 1))
 	else:
@@ -126,10 +128,11 @@ def main():
 	def_equipment_bonus = 50
 	attack_roll = calcAttackRoll(effective_level, equipment_bonus)
 	defence_roll = calcDefenceRoll(def_effective_level, def_equipment_bonus)
-	# accuracy = calcHitChance(attack_roll, defence_roll)
+	# accuracy = calcHitChance(attack_roll, defence_roll, True)
 	hits = []
 	for i in range(100000):
-		hits.append(calcHitChance(attack_roll, defence_roll))
+		hits.append(calcHitChance(attack_roll, defence_roll, True))
 	print(sum(hits) / len(hits))
+
 if __name__ == '__main__':
 	main()
