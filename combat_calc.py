7218d927cf6f1c011ebc906abca401d33ba38d55
Farhan Ahmad <fahmad010200@gmail.com>
2020-08-26 19:24:32 +0100

    BGS Inq Fix#

--- combat_calc.py
+++ combat_calc.py
@@ -1,166 +1,182 @@
-#Created by @Phoneman_btw
+# Created by @Phoneman_btw
 import random
 
+
 class NPC:
-	"""docstring for NPC"""
-	def __init__(self, hp, defence, mage, stab_def, slash_def, crush_def, mage_def, range_def):
-		self.hp = hp
-		self.defence = defence
-		self.mage = mage
-		self.stab_def = stab_def
-		self.slash_def = slash_def
-		self.crush_def = crush_def
-		self.mage_def = mage_def
-		self.range_def = range_def
-	#Add regen when I can be asked
+    """docstring for NPC"""
 
-	def lower_hp(self, damage):
-		self.hp -= damage
-		if self.hp < 0:
-			self.hp = 0
+    def __init__(self, hp, defence, mage, stab_def, slash_def, crush_def, mage_def, range_def):
+        self.hp = hp
+        self.defence = defence
+        self.mage = mage
+        self.stab_def = stab_def
+        self.slash_def = slash_def
+        self.crush_def = crush_def
+        self.mage_def = mage_def
+        self.range_def = range_def
 
-	def lower_def(self, amount):
-		self.defence -= amount
-		if self.defence < 0:
-			self.defence = 0
-	
-		
+    # Add regen when I can be asked
+
+    def lower_hp(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+
+    def lower_def(self, amount):
+        self.defence -= amount
+        if self.defence < 0:
+            self.defence = 0
+
 
 def simHit(accuracy, maxHit):
-	accuracy_rand = random.randint(0,10000)
-	hit = random.randint(0, maxHit)
-	if accuracy * 10000 >= accuracy_rand:
-		return hit
-	else:
-		return 0
+    accuracy_rand = random.randint(0, 10000)
+    hit = random.randint(0, maxHit)
+    if accuracy * 10000 >= accuracy_rand:
+        return hit
+    else:
+        return 0
+
 
 def simScytheHit(accuracy, maxHit):
-	final_hit = 0
-	final_hit += simHit(accuracy, maxHit)
-	final_hit += simHit(accuracy, multiplyDown(maxHit, 0.5))
-	final_hit += simHit(accuracy, multiplyDown(maxHit, 0.25))
-	return final_hit
+    final_hit = 0
+    final_hit += simHit(accuracy, maxHit)
+    final_hit += simHit(accuracy, multiplyDown(maxHit, 0.5))
+    final_hit += simHit(accuracy, multiplyDown(maxHit, 0.25))
+    return final_hit
 
-#This section is for melee and ranged max hit
 
-def multiplyDown(number, multiplier): #Increases the number by multiplier then floors it lmao
-	number *= multiplier
-	number = int(number)
-	return number
+# This section is for melee and ranged max hit
 
-def calcMaxHit(effective_strength, equipment_strength, set_bonus = "", special_attack = "", mage_level = 0):
-	maxHit = int(0.5 + effective_strength * (equipment_strength+64)/640)
-	if set_bonus == "Inquisitor":
-		maxHit = multiplyDown(maxHit, 1.025)
-	if special_attack == "Bandos godsword":
-		maxHit = multiplyDown(maxHit, 1.10)
-	elif special_attack == "Dragon warhammer":
-		maxHit = multiplyDown(maxHit, 1.50)
-	elif special_attack == "Twisted bow":
-		maxHit = multiplyDown(maxHit, (0.5386 + 0.0087 * mage_level - 0.000009 * mage_level**2))
-	return maxHit
+def multiplyDown(number, multiplier):  # Increases the number by multiplier then floors it lmao
+    number *= multiplier
+    number = int(number)
+    return number
 
-def getEffectiveStrength(combat, visible_level, style, prayer = "none", set_bonus = "none"):
-	if prayer == "Piety":
-		visible_level *= 1.23
-	elif prayer == "Rigour":
-		visible_level *= 1.23
-	visible_level = int(visible_level)
 
-	if style == "Aggressive":
-		visible_level += 3
-	elif style == "Controlled":
-		visible_level += 1
-	elif style == "Accurate":
-		if combat == "Ranged":
-			visible_level += 3
+def calcMaxHit(effective_strength, equipment_strength, set_bonus="", special_attack="", mage_level=0):
+    maxHit = int(0.5 + effective_strength * (equipment_strength + 64) / 640)
+    if set_bonus == "Inquisitor":
+        maxHit = multiplyDown(maxHit, 1.025)
+    if special_attack == "Bandos godsword":
+        maxHit = multiplyDown(maxHit, 1.1)
+        maxHit = multiplyDown(maxHit, 1.1)
+    elif special_attack == "Dragon warhammer":
+        maxHit = multiplyDown(maxHit, 1.50)
+    elif special_attack == "Twisted bow":
+        maxHit = multiplyDown(maxHit, (0.5386 + 0.0087 * mage_level - 0.000009 * mage_level ** 2))
+    return maxHit
 
-	visible_level += 8
-	if set_bonus == "Regular Void":
-		visible_level = multiplyDown(visible_level, 1.1)
-	elif set_bonus == "Elite Void":
-		if combat == "Ranged":
-			visible_level = multiplyDown(visible_level, 1.125)
-	return visible_level
 
-#This section is for magic max hit
+def getEffectiveStrength(combat, visible_level, style, prayer="none", set_bonus="none"):
+    if prayer == "Piety":
+        visible_level *= 1.23
+    elif prayer == "Rigour":
+        visible_level *= 1.23
+    visible_level = int(visible_level)
 
-def calcMageMaxHit(spell = "Sang", visible_mage = 120, magic_boost = 0, tome = True):
-	magic_boost /= 100
-	if spell == "Sang":
-		maxHit = int(visible_mage / 3) - 1
-	elif spell == "Fire Surge":
-		maxHit = 24
-	maxHit = multiplyDown(maxHit, 1 + magic_boost)
-	if spell == "Fire Surge" and tome == True:
-		maxHit = multiplyDown(maxHit, 1.5)
-	return maxHit
+    if style == "Aggressive":
+        visible_level += 3
+    elif style == "Controlled":
+        visible_level += 1
+    elif style == "Accurate":
+        if combat == "Ranged":
+            visible_level += 3
 
-#This section is for calculating accuracy
+    visible_level += 8
+    if set_bonus == "Regular Void":
+        visible_level = multiplyDown(visible_level, 1.1)
+    elif set_bonus == "Elite Void":
+        if combat == "Ranged":
+            visible_level = multiplyDown(visible_level, 1.125)
+    return visible_level
 
-def calcAttackRoll(effective_level, equipment_bonus, set_bonus = "", special_attack = "", mage_level = 0): #Take the corresponding stab, slash, crush, ranged or magic attack bonus from the equipment stats interface and let this equal equipment_bonus
-	max_roll = effective_level * (equipment_bonus + 64)
-	if set_bonus == "Inquisitor":
-		max_roll = multiplyDown(max_roll, 1.025)
-	if special_attack == "Bandos godsword":
-		max_roll = multiplyDown(max_roll, 2.00)
-	elif special_attack == "Twisted bow":
-		max_roll = multiplyDown(max_roll, min((0.399 + 0.0063 * min((350, mage_level)) - 0.000009 * min((350, mage_level))**2), 1.4)) #Bow accuracy capped at 1.4
-	return max_roll
+
+# This section is for magic max hit
+
+def calcMageMaxHit(spell="Sang", visible_mage=120, magic_boost=0, tome=True):
+    magic_boost /= 100
+    if spell == "Sang":
+        maxHit = int(visible_mage / 3) - 1
+    elif spell == "Fire Surge":
+        maxHit = 24
+    maxHit = multiplyDown(maxHit, 1 + magic_boost)
+    if spell == "Fire Surge" and tome == True:
+        maxHit = multiplyDown(maxHit, 1.5)
+    return maxHit
+
+
+# This section is for calculating accuracy
+
+def calcAttackRoll(effective_level, equipment_bonus, set_bonus="", special_attack="",
+                   mage_level=0):  # Take the corresponding stab, slash, crush, ranged or magic attack bonus from the equipment stats interface and let this equal equipment_bonus
+    max_roll = effective_level * (equipment_bonus + 64)
+    if set_bonus == "Inquisitor":
+        max_roll = multiplyDown(max_roll, 1.025)
+    if special_attack == "Bandos godsword":
+        max_roll = multiplyDown(max_roll, 2.00)
+    elif special_attack == "Twisted bow":
+        max_roll = multiplyDown(max_roll,
+                                min((0.399 + 0.0063 * min((350, mage_level)) - 0.000009 * min((350, mage_level)) ** 2),
+                                    1.4))  # Bow accuracy capped at 1.4
+    return max_roll
+
 
 def calcDefenceRoll(effective_level, equipment_bonus):
-	max_roll = effective_level * (equipment_bonus + 64)
-	return max_roll
-
-def calcHitChance(max_attack_roll, max_defence_roll, brimstone = False):
-	if brimstone:
-		brim_proc = random.randint(1,4)
-		if brim_proc == 1:
-			max_defence_roll = multiplyDown(max_defence_roll, 0.9)
-	if max_attack_roll > max_defence_roll:
-		hit_chance = 1 - (max_defence_roll + 2) / (2 * (max_attack_roll + 1))
-	else:
-		hit_chance = max_attack_roll / (2 * (max_defence_roll + 1))
-	return hit_chance
+    max_roll = effective_level * (equipment_bonus + 64)
+    return max_roll
 
 
-def getEffectiveLevel(visible_level, style = "Controlled", combat = "", prayer = "", set_bonus = ""): #Monsters are basically always on controlled for calculations sake
-	effective_level = visible_level
-	if prayer == "Piety":
-		effective_level = multiplyDown(effective_level, 1.2)
-	elif prayer == "Rigour":
-		effective_level = multiplyDown(effective_level, 1.2)
-	elif prayer == "Augury":
-		effective_level = multiplyDown(effective_level, 1.25)
-	if style =="Accurate":
-		effective_level += 3
-	elif style == "Controlled":
-		effective_level += 1
-	elif style == "Long range":
-		if combat == "Magic":
-			effective_level += 1
-	effective_level += 8
-	if set_bonus == "Regular Void":
-		if combat == "Melee" or "Ranged":
-			effective_level = multiplyDown(effective_level, 1.10)
-		elif combat == "Magic":
-			effective_level = multiplyDown(effective_level, 1.45)
-	return effective_level
+def calcHitChance(max_attack_roll, max_defence_roll, brimstone=False):
+    if brimstone:
+        brim_proc = random.randint(1, 4)
+        if brim_proc == 1:
+            max_defence_roll = multiplyDown(max_defence_roll, 0.9)
+    if max_attack_roll > max_defence_roll:
+        hit_chance = 1 - (max_defence_roll + 2) / (2 * (max_attack_roll + 1))
+    else:
+        hit_chance = max_attack_roll / (2 * (max_defence_roll + 1))
+    return hit_chance
+
+
+def getEffectiveLevel(visible_level, style="Controlled", combat="", prayer="",
+                      set_bonus=""):  # Monsters are basically always on controlled for calculations sake
+    effective_level = visible_level
+    if prayer == "Piety":
+        effective_level = multiplyDown(effective_level, 1.2)
+    elif prayer == "Rigour":
+        effective_level = multiplyDown(effective_level, 1.2)
+    elif prayer == "Augury":
+        effective_level = multiplyDown(effective_level, 1.25)
+    if style == "Accurate":
+        effective_level += 3
+    elif style == "Controlled":
+        effective_level += 1
+    elif style == "Long range":
+        if combat == "Magic":
+            effective_level += 1
+    effective_level += 8
+    if set_bonus == "Regular Void":
+        if combat == "Melee" or "Ranged":
+            effective_level = multiplyDown(effective_level, 1.10)
+        elif combat == "Magic":
+            effective_level = multiplyDown(effective_level, 1.45)
+    return effective_level
 
 
 def main():
-	max_hit = calcMageMaxHit("Sang", 99)
-	effective_level = getEffectiveLevel(99, "Accurate", "Magic")
-	def_effective_level = getEffectiveLevel(250)
-	equipment_bonus = 31
-	def_equipment_bonus = 50
-	attack_roll = calcAttackRoll(effective_level, equipment_bonus)
-	defence_roll = calcDefenceRoll(def_effective_level, def_equipment_bonus)
-	# accuracy = calcHitChance(attack_roll, defence_roll, True)
-	hits = []
-	for i in range(100000):
-		hits.append(calcHitChance(attack_roll, defence_roll, True))
-	print(sum(hits) / len(hits))
+    max_hit = calcMageMaxHit("Sang", 99)
+    effective_level = getEffectiveLevel(99, "Accurate", "Magic")
+    def_effective_level = getEffectiveLevel(250)
+    equipment_bonus = 31
+    def_equipment_bonus = 50
+    attack_roll = calcAttackRoll(effective_level, equipment_bonus)
+    defence_roll = calcDefenceRoll(def_effective_level, def_equipment_bonus)
+    # accuracy = calcHitChance(attack_roll, defence_roll, True)
+    hits = []
+    for i in range(100000):
+        hits.append(calcHitChance(attack_roll, defence_roll, True))
+    print(sum(hits) / len(hits))
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- olm_sim.py
+++ olm_sim.py
@@ -1,6 +1,8 @@
 from combat_calc import *
-from statistics import mean,mode,median
-#Olm procedure
+from statistics import mean, mode, median
+
+
+# Olm procedure
 #	Everyone ovl'd once going in
 #	specs on melee hand (3 dwh, 4 bgs)
 #	Kill mage hand
@@ -17,65 +19,88 @@ from statistics import mean,mode,median
 #	Range Olm
 
 def sangHit():
-	pass
+    pass
+
 
 def inqHammer(NPC):
-	att_effective_level = getEffectiveLevel(120, "Accurate", "Melee", "Piety")
-	def_effective_level = getEffectiveLevel(NPC.defence)
-	attack_roll = calcAttackRoll(att_effective_level, 192, "Inquisitor")
-	defence_roll = calcDefenceRoll(def_effective_level, 50)
-	accuracy = calcHitChance(attack_roll, defence_roll)
-	effective_strength = getEffectiveStrength("Melee", 120, "Accurate", "Piety")
-	max_hit = calcMaxHit(effective_strength, 148, "Inquisitor", "Dragon warhammer")
-	return accuracy, max_hit
+    att_effective_level = getEffectiveLevel(120, "Accurate", "Melee", "Piety")
+    def_effective_level = getEffectiveLevel(NPC.defence)
+    attack_roll = calcAttackRoll(att_effective_level, 192, "Inquisitor")
+    defence_roll = calcDefenceRoll(def_effective_level, 50)
+    accuracy = calcHitChance(attack_roll, defence_roll)
+    effective_strength = getEffectiveStrength("Melee", 118, "Accurate", "Piety")
+    max_hit = calcMaxHit(effective_strength, 183, "Inquisitor", "Dragon warhammer")
+    return accuracy, max_hit
+
 
 def inqBGS(NPC):
-	att_effective_level = getEffectiveLevel(120, "Aggressive", "Melee", "Piety")
-	def_effective_level = getEffectiveLevel(NPC.defence)
-	attack_roll = calcAttackRoll(att_effective_level, 149, "Inquisitor", "Bandos godsword")
-	defence_roll = calcDefenceRoll(def_effective_level, 50)
-	accuracy = calcHitChance(attack_roll, defence_roll)
-	effective_strength = getEffectiveStrength("Melee", 120, "Aggressive", "Piety")
-	max_hit = calcMaxHit(effective_strength, 191, "Inquisitor", "Bandos godsword")
-	print(accuracy, max_hit)
-	return accuracy, max_hit
+    att_effective_level = getEffectiveLevel(120, "Aggressive", "Melee", "Piety")
+    def_effective_level = getEffectiveLevel(NPC.defence)
+    attack_roll = calcAttackRoll(att_effective_level, 149, "Inquisitor", "Bandos godsword")
+    defence_roll = calcDefenceRoll(def_effective_level, 50)
+    accuracy = calcHitChance(attack_roll, defence_roll)
+    effective_strength = getEffectiveStrength("Melee", 120, "Aggressive", "Piety")
+    max_hit = calcMaxHit(effective_strength, 183, "Inquisitor", "Bandos godsword")
+    return accuracy, max_hit
 
-#(self, hp, defence, mage, stab_def, slash_def, crush_def, mage_def, range_def):
+
+# (self, hp, defence, mage, stab_def, slash_def, crush_def, mage_def, range_def):
 
 def main():
-	olm_mage_hand = NPC(2400, 185, 92, 200, 200, 200, 50, 200)
-	olm_head = NPC(3200, 159, 300, 200, 200, 200, 200, 50)
-	olm_melee_hand = NPC(2400, 185, 185, 50, 50, 50, 200, 200)
-	defences = []
-	inqBGS(olm_melee_hand)
-	# for i in range(10000):
-	# 	olm_melee_hand = NPC(2400, 185, 185, 50, 50, 50, 200, 200)
-	# 	accuracy, max_hit = inqHammer(olm_melee_hand)
+    olm_mage_hand = NPC(2400, 185, 92, 200, 200, 200, 50, 200)
+    olm_head = NPC(3200, 159, 300, 200, 200, 200, 200, 50)
+    olm_melee_hand = NPC(2400, 185, 185, 50, 50, 50, 200, 200)
+    defences = []
 
-	# 	damage1 = simHit(accuracy, max_hit)
-	# 	damage2 = simHit(accuracy, max_hit)
-	# 	damage3 = simHit(accuracy, max_hit)
+    for i in range(1000000):
+        olm_melee_hand = NPC(2400, 185, 185, 50, 50, 50, 200, 200)
+        accuracy, max_hit = inqHammer(olm_melee_hand)
 
-	# 	if damage1 > 0:
-	# 		olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
-	# 	if damage2 > 0:
-	# 		olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
-	# 	if damage3 > 0:
-	# 		olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
-	# 	defences.append(olm_melee_hand.defence)
-	# print("Mode:", mode(defences))
-	# print("Median:", median(defences))
-	# print("Mean", mean(defences))
-		# print(olm_melee_hand.defence)
-	#def calcAttackRoll(effective_level, equipment_bonus, special_attack = "", mage_level = 0)
-	#def calcDefenceRoll(effective_level, equipment_bonus):
-	#def calcHitChance(max_attack_roll, max_defence_roll, brimstone = False):
-	#calcMaxHit(effective_strength, equipment_strength, set_bonus = "", special_attack = "", mage_level = 0)
-	#getEffectiveStrength(combat, visible_level, style, prayer = "none", set_bonus = "none"):
-	#getEffectiveLevel(visible_level, style = "Controlled", combat = "", prayer = "", set_bonus = "")
+        damage1 = simHit(accuracy, max_hit)
+        damage2 = simHit(accuracy, max_hit)
+        #damage3 = simHit(accuracy, max_hit)
+
+        if damage1 > 0:
+            olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
+        if damage2 > 0:
+            olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
+        #if damage3 > 0:
+        #    olm_melee_hand.lower_def(multiplyDown(olm_melee_hand.defence, 0.3))
+
+        accuracy, max_hit = inqBGS(olm_melee_hand)
+
+        damage3 = simHit(accuracy, max_hit)
+        damage4 = simHit(accuracy, max_hit)
+        damage5 = simHit(accuracy, max_hit)
+        damage6 = simHit(accuracy, max_hit)
+        damage7 = simHit(accuracy, max_hit)
+
+        olm_melee_hand.lower_def(damage3)
+        olm_melee_hand.lower_def(damage4)
+        olm_melee_hand.lower_def(damage5)
+        olm_melee_hand.lower_def(damage6)
+        olm_melee_hand.lower_def(damage7)
+
+        defences.append(olm_melee_hand.defence)
+
+    print("Mode:", mode(defences))
+    print("Median:", median(list(filter(lambda a: a != 0, defences))))
+    print("Mean", mean(defences))
+    zero_def_rate = round((defences.count(0) / len(defences) * 100), 2)
+    print("amount of times at 0 defence: "+str(defences.count(0)))
+
+    print("That's "+str(zero_def_rate)+"% of the time, or 1/"+str(round((defences.count(0) /  len(defences)) ** -1, 2)))
 
 
+
+# print(olm_melee_hand.defence)
+# def calcAttackRoll(effective_level, equipment_bonus, special_attack = "", mage_level = 0)
+# def calcDefenceRoll(effective_level, equipment_bonus):
+# def calcHitChance(max_attack_roll, max_defence_roll, brimstone = False):
+# calcMaxHit(effective_strength, equipment_strength, set_bonus = "", special_attack = "", mage_level = 0)
+# getEffectiveStrength(combat, visible_level, style, prayer = "none", set_bonus = "none"):
+# getEffectiveLevel(visible_level, style = "Controlled", combat = "", prayer = "", set_bonus = "")
 
 
 if __name__ == '__main__':
-	main()
+    main()
