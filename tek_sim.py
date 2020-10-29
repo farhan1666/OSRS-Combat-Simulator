b4e4efbf87860fe689ec508bc70a9e35680a5a4a
farhan1666 <36961985+farhan1666@users.noreply.github.com>
2020-10-29 14:19:30 +0000

    Tek Sim but it actually gives useful information
    The findings of this code can be found here: https://discordapp.com/channels/408082929940430858/411330692228841483/764319653233033216
    Alternatively: https://i.imgur.com/HXVJ1ZC.png

--- tek_sim.py
+++ tek_sim.py
@@ -11,6 +11,18 @@ def inqHammer(NPC):
 	max_hit = calcMaxHit(effective_strength, 148, "Inquisitor", "Dragon warhammer")
 	return accuracy, max_hit
 
+def Hammer(NPC):
+	att_effective_level = getEffectiveLevel(118, "Accurate", "Melee", "Piety")
+	def_effective_level = getEffectiveLevel(NPC.defence)
+	attack_roll = calcAttackRoll(att_effective_level, 160)
+	defence_roll = calcDefenceRoll(def_effective_level, NPC.crush_def)
+	accuracy = calcHitChance(attack_roll, defence_roll)
+
+	effective_strength = getEffectiveStrength("Melee", 118, "Accurate", "Piety")
+	max_hit = calcMaxHit(effective_strength, 150, "", "Dragon warhammer")
+	return accuracy, max_hit
+
+
 
 def inqBGS(NPC):
 	att_effective_level = getEffectiveLevel(118, "Aggressive", "Melee", "Piety")
@@ -25,43 +37,105 @@ def inqBGS(NPC):
 
 def main():
 	# (self, hp, defence, mage, stab_def, slash_def, crush_def, mage_def, range_def):
-	defences = []
+	tekton_hp_inq = []
 
-	for i in range(40000):
-		tekton = NPC(1200, 217, 217, 155, 165, 105, 0, 0)
+	for i in range(100000):
+		tekton = NPC(300, 205, 205, 155, 165, 105, 0, 0)
 		accuracy, max_hit = inqHammer(tekton)
 
+		venghit1 = simHit(1, 51)
+		venghit2 = simHit(1, 51)
+
+		tekton.lower_hp(int((venghit1+1)*.75))
+		tekton.lower_hp(int((venghit2+1)*.75))
+
 		damage1 = simHit(accuracy, max_hit)
-		damage2 = simHit(accuracy, max_hit)
 
 		if damage1 > 0:
 			tekton.lower_def(multiplyDown(tekton.defence, 0.3))
+		if damage1 == 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.05))
+
+		accuracy, max_hit = inqHammer(tekton)
+		damage2 = simHit(accuracy, max_hit)
+
 		if damage2 > 0:
 			tekton.lower_def(multiplyDown(tekton.defence, 0.3))
+		if damage2 == 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.05))
 
-		accuracy, max_hit = inqBGS(tekton)
+		tekton.lower_hp(damage1)
+		tekton.lower_hp(damage2)
+		att_effective_level = getEffectiveLevel(118, "Aggressive", "Melee", "Piety")
+		def_effective_level = getEffectiveLevel(tekton.defence)
+		attack_roll = calcAttackRoll(att_effective_level, 99, "Inquisitor")
+		defence_roll = calcDefenceRoll(def_effective_level, tekton.crush_def)
+		accuracy = calcHitChance(attack_roll, defence_roll)
+		effective_strength = getEffectiveStrength("Melee", 118, "Aggressive", "Piety")
+		max_hit = calcMaxHit(effective_strength, 130, "inquisitor", "")
 
-		damage3 = simHit(accuracy, max_hit)
-		damage4 = simHit(accuracy, max_hit)
-		damage5 = simHit(accuracy, max_hit)
-		damage6 = simHit(accuracy, max_hit)
-		damage7 = simHit(accuracy, max_hit)
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton_hp_inq.append(tekton.hp)
 
-		tekton.lower_def(damage3)
-		tekton.lower_def(damage4)
-		tekton.lower_def(damage5)
-		tekton.lower_def(damage6)
-		tekton.lower_def(damage7)
 
-		defences.append(tekton.defence)
+	tekton_hp_bandos = []
+	for i in range(100000):
+		tekton = NPC(300, 205, 205, 155, 165, 105, 0, 0)
 
-	print("Mode:", mode(defences))
-	print("Median:", median(defences))
-	print("Mean", mean(defences))
-	zero_def_rate = round((defences.count(0) / len(defences) * 100), 2)
-	print("Amount of times at 0 defence: "+str(defences.count(0)))
+		venghit1 = simHit(1, 51)
+		venghit2 = simHit(1, 51)
 
-	print("That's "+str(zero_def_rate)+"% of the time, or 1/"+str(round((defences.count(0) /  len(defences)) ** -1, 2)))
+		tekton.lower_hp(int((venghit1+1)*.75))
+		tekton.lower_hp(int((venghit2+1)*.75))
+
+		accuracy, max_hit = Hammer(tekton)
+
+		damage1 = simHit(accuracy, max_hit)
+
+
+		if damage1 > 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.3))
+		if damage1 == 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.05))
+		accuracy, max_hit = Hammer(tekton)
+		damage2 = simHit(accuracy, max_hit)
+		if damage2 > 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.3))
+		if damage2 == 0:
+			tekton.lower_def(multiplyDown(tekton.defence, 0.05))
+
+		tekton.lower_hp(damage1)
+		tekton.lower_hp(damage2)
+		att_effective_level = getEffectiveLevel(118, "Aggressive", "Melee", "Piety")
+		def_effective_level = getEffectiveLevel(tekton.defence)
+		attack_roll = calcAttackRoll(att_effective_level, 147, "")
+		defence_roll = calcDefenceRoll(def_effective_level, tekton.slash_def)
+		accuracy = calcHitChance(attack_roll, defence_roll)
+		effective_strength = getEffectiveStrength("Melee", 118, "Aggressive", "Piety")
+		max_hit = calcMaxHit(effective_strength, 132, "", "")
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton.lower_hp(simScytheHit(accuracy, max_hit))
+		tekton_hp_bandos.append(tekton.hp)
+
+
+
+	print("inq: ", str(tekton_hp_inq.count(0)))
+
+	print("Bandos: ", str(tekton_hp_bandos.count(0)))
+
+	print("inq rate: 1/", str(100000/tekton_hp_inq.count(0)))
+	print("Bandos rate: 1/", str(100000/tekton_hp_bandos.count(0)))
+
+
 
 
 
